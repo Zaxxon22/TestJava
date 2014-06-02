3a154a6341e27790f50f99734105f7d04ceb6816
Zaxxon22 <julien.valgres@gmail.com>
2014-06-02 10:58:15 +0200

    testn edition debut

--- src/poker_tests/Paquet.java
+++ src/poker_tests/Paquet.java
@@ -1,5 +1,19 @@
 package poker_tests;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Paquet {
+	
+	private List<Carte> deck;
+	private int position;
+
+	public Paquet() {
+		
+		deck = new LinkedList<Carte>();
+		
+	}
+	
+	
 
 }
