b683179ea429ae00a262ca31cf0b8c7990b3c95b
Zaxxon22 <julien.valgres@gmail.com>
2014-06-02 14:20:03 +0200

    ...

--- src/poker_tests/Paquet.java
+++ src/poker_tests/Paquet.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class Paquet {
+public class Paquet {//
 	
 	private List<Carte> deck;
 	private int position;
